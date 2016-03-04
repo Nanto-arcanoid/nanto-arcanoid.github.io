a7372af2c7a77cec663a46d5eda1875da7e574de
saakyan <saakyan@competentum.ru>
2016-03-04 15:47:49 +0300

    add ball

--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
 
+	var widthBox = 500;
+	var heightBox = 600;
+	var widthBall = 30;
+	var heightBall = 30;
+	var innerWidth = widthBox - widthBall;
+	var innerHeight = heightBox - heightBall;
+	var posX = 70;
+	var posY = 40;
+	var shiftX = 1;
+	var shiftY = 1;
+	$("#box").css("width", widthBox).css("height", heightBox);
+	$("#ball.startpos").css("width", widthBall).css("height", heightBall).css("left", posX).css("bottom", posY);
+
+
 	var box = $("#box"),
 		boxOffset = box.offset(),
 		mouseCoords = boxOffset.left,
 		time,
-		speed;
+		speed,
+		ratioFirstStrike = 1;
 
 	$("body").on("mousemove", box, function(e) {
 		mouseCoords = Math.round((e.pageX - boxOffset.left) / 10) * 10 - 50;
 	});
-	
-	if (mouseCoords >= 405) {mouseCoords = 405};
-	if (mouseCoords <= -5) {mouseCoords = -5};
+
 
 	animate();
 
 	function move() {
+			if (mouseCoords >= (innerWidth + 5 - 100)) {mouseCoords = (innerWidth + 5 -100)};
+			if (mouseCoords <= -5) {mouseCoords = -5};
+			
 			var now = new Date().getTime(),
 				dt = (now - (time || now)) / 4;
+				dtBall = (now - (time || now)) / 2;
 				time = now,
 				oldPos = parseFloat($("#mover").css("left")),
 				tempPos = oldPos,
@@ -26,12 +43,47 @@ $(document).ready(function(){
 			if (newPos > 10) {
 				tempPos += dt;
 				$("#mover").css("left", tempPos);
+				$("#ball.startpos").css("left", tempPos + 70);
 			} else if (newPos < -10) {
 				tempPos -= dt;
 				$("#mover").css("left", tempPos);
+				$("#ball.startpos").css("left", tempPos + 70);
 			} else {
 				$("#mover").css("left", tempPos);
+				$("#ball.startpos").css("left", tempPos + 70);
 			};
+
+			posX = parseInt($("#ball").css("left"));
+				posY = parseInt($("#ball").css("bottom"));
+				//console.log(posX, posY);
+
+				if(posX > innerWidth) {
+					posX = innerWidth;
+					shiftX = -(shiftX);
+				};
+				if(posY > innerHeight) {
+					posY = innerHeight;
+					shiftY = -(shiftY);
+				};
+				if(posX < 0) {
+					posX = 0;
+					shiftX = -(shiftX);
+				};
+				if(posY < 0) {
+					posY = 0;
+					shiftY = -(shiftY);
+				};
+				posX += (shiftX * dtBall);
+				posY += (shiftY * dtBall);
+
+				$("#ball.movepos").css("left", posX).css("bottom", posY);
+
+
+			$("body").on("click", box, function() {
+				$("#ball").removeClass("startpos");
+				$("#ball").addClass("movepos");
+			});
+
 	}
 
 	function animate() {
@@ -51,3 +103,7 @@ window.requestAnimFrame = (function() {
 				window.setTimeout(callback, 1000 / 60);
 			};
 })();
+
+
+
+
